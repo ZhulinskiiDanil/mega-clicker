474ce06dc11ba144e82df5898e672209154f0fc3
ZhulinskiiDanil <zhulinskiidanil@gmail.com>
2023-10-05 08:14:54 +0300

    fix: fix react key prop

--- src/entities/road/item/ui/index.tsx
+++ src/entities/road/item/ui/index.tsx
@@ -43,7 +43,7 @@ export function Item({
         </p>
         <div className={styles.tags}>
           {(tags || []).map(tag => (
-            <div className={styles.tag}>
+            <div key={tag} className={styles.tag}>
               { tag }
             </div>
           ))}
