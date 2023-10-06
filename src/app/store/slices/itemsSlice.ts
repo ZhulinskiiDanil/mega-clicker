7c002b78eac37a2af8144637131d02075f14f254
ZhulinskiiDanil <zhulinskiidanil@gmail.com>
2023-10-06 19:47:22 +0300

    build: fix 'self is not defined' error

--- src/app/store/slices/itemsSlice.ts
+++ src/app/store/slices/itemsSlice.ts
@@ -1,6 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const uuid = () => self.crypto.randomUUID()
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 const item = (props: { from?: number } = {}) => ({
   id: uuid(),
   imageURL: '/images/skins/AK47_Vulcan.png',
